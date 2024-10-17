3850dc23f04471ce7b492363ec2a0e034af1af41
crowdbotics-apps <team@crowdbotics.com>
2024-10-17 17:53:58 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Yvcvd
+
+admin.site.register(Yvcvd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Yvcvd(models.Model):
+    "Generated Model"
+    obcbhd = models.BigIntegerField()
+    bchdjs = models.BigIntegerField()
